04b1070a7f64fd28fc6398e2dca39d94dc378a91
adeliadev <arodriguesthamyres@gmail.com>
2024-10-27 15:01:46 -0300

    feat: Add Task entity

---
+++ src/main/java/br/com/adeliadev/to_do_list/model/Task.java
@@ -0,0 +1,21 @@
+package br.com.adeliadev.to_do_list.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private boolean completed;
+
+}
